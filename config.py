d2d1093a172caab7f78609ba2754a4be88203ddf
henry-hft <333333henry@gmail.com>
2026-03-27 04:15:35 -0700

    Add default Z positions and lens positions to config

--- config.py
+++ config.py
@@ -2,3 +2,8 @@ MODEL_PATH = "./cnn/model_screws.keras"
 LABELS_PATH = "./files/object_classes.json"
 DOBOT_MODES = "./files/dobot_modes.json"
 IMAGE_DIR = "./images/detail/"
+
+GRIP_Z_POSITION = -131.4 # Default: -138
+DETAIL_IMAGE_Z_POSITION = -115 # Default: -125
+OVERVIEW_IMAGE_LENS_POSITION = 2.0 # Default: 2.0
+DETAIL_IMAGE_LENS_POSITION = 10.0 # Default: 10.0
